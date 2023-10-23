7378e95e4c452bbfc3b1c72f6cb2b47b7e5e83a6
Martin Rosas <mlrosas21@hotmail.com>
2023-10-23 19:58:52 -0300

    refactoring links

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import logo from "@assets/logo.png";
 import Image from "next/image";
 import SearchBar from "../SearchBar";
+import NavLink from "../NavLink/NavLink";
 
 const NavBar = () => {
   return (
@@ -20,34 +21,21 @@ const NavBar = () => {
             <div className="flex items-center space-x-4 justify-between">
               <SearchBar />
               <nav className="flex space-x-4">
-                <Link href="/">Log in</Link>
-                <Link href="/about">Sign up</Link>
-                <Link href="/contact">Cart</Link>
+                <NavLink route="/" label="Log in" />
+                <NavLink route="/" label="Sign up" />
+                <NavLink route="/" label="Cart" />
               </nav>
             </div>
-            <nav>
-              <ul className="flex space-x-4">
-                <li>
-                  <Link href="/">Categories</Link>
-                </li>
-                <li>
-                  <Link href="/about">Popular Models</Link>
-                </li>
-                <li>
-                  <Link href="/contact">On Sale</Link>
-                </li>
-                <li>
-                  <Link href="/contact">Customer Service</Link>
-                </li>
-                <li>
-                  <Link href="/contact">About us</Link>
-                </li>
-              </ul>
+            <nav className="flex space-x-4 mt-2">
+              <NavLink label="Categories" route="/" />
+              <NavLink label="Popular Models" route="/about" />
+              <NavLink label="On Sale" route="/contact" />
+              <NavLink label="Customer Service" route="/contact" />
+              <NavLink label="About us" route="/contact" />
             </nav>
           </div>
         </div>
       </div>
-      <div></div>
     </header>
   );
 };
