dafcb0e1afc8da62765dff6c96a9963df0b13f60
Jeisson Lopez <jeisonma86@hotmail.com>
2023-10-24 19:44:52 -0500

    PI2G1-8 Adición de los  botones de add to cart y like en la tarjeta

--- src/services/costumes_mock.service.ts
+++ src/services/costumes_mock.service.ts
@@ -2,7 +2,7 @@ export const getCostume = async (costumeId: number) => {
     return {
         id: costumeId,
         name: "Elsa from Frozen",
-        price: 46.00,
+        price: "46.00",
         url_image: "https://images.unsplash.com/photo-1607824693178-8dcc03b4ac74?auto=format&fit=crop&q=80&w=2145&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         sizes: [
             {
