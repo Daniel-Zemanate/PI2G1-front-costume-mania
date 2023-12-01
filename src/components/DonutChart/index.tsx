6f0f59b345c5b7f1e72d38d1713995ba0b0b9ab7
Martin Rosas <mlrosas21@hotmail.com>
2023-11-30 23:40:35 -0300

    fix pie chart admin report

--- src/components/DonutChart/index.tsx
+++ src/components/DonutChart/index.tsx
@@ -4,8 +4,6 @@ import {
   Pie,
   PieChart,
   ResponsiveContainer,
-  PieLabelRenderProps,
-  Tooltip,
   Label,
 } from "recharts";
 
@@ -16,30 +14,38 @@ type Props = {
   };
 };
 
-const QUANTITIES = ["quantitySales", "quantityDeliveredSales"];
-
 const DonutChart: FC<Props> = ({ data }) => {
-  const quantitiesArray = Object.entries(data)
-    .filter(([key, value]) => QUANTITIES.includes(key))
-    .map(([key, value]) => ({
-      name: key,
-      value: value,
-    }));
-  
-  const deliveredPercentage = ((data.quantityDeliveredSales / data.quantitySales)*100).toFixed(2)
+  const quantityNotDeliveredSales =
+    data.quantitySales - data.quantityDeliveredSales;
+
+  const graphData = [
+    {
+      name: "quantityDeliveredSales",
+      value: data.quantityDeliveredSales,
+    },
+    {
+      name: "quantityNotDeliveredSales",
+      value: quantityNotDeliveredSales,
+    },
+  ];
+
+  const deliveredPercentage = (
+    (data.quantityDeliveredSales / data.quantitySales) *
+    100
+  ).toFixed(2);
 
   return (
     <ResponsiveContainer width={"100%"} height={"100%"} className="p-2">
       <PieChart width={300} height={300}>
         <Pie
-          data={quantitiesArray}
-          dataKey="value" 
+          data={graphData}
+          dataKey="value"
           innerRadius={60}
           outerRadius={80}
         >
-          {quantitiesArray.map((entry, index) => {
+          {graphData.map((entry, index) => {
             if (index === 1) {
-              return <Cell key={`cell-${index}`} fill="#f3f6f9" />; // make sure to map the index to the colour you want
+              return <Cell key={`cell-${index}`} fill="#f3f6f9" />;
             }
             return <Cell key={`cell-${index}`} fill="#B71FD0" />;
           })}
