c2e62cd2bcc3ef2af8d6e633a71253927d05dc4a
Martin Rosas <mlrosas21@hotmail.com>
2023-12-01 20:10:47 -0300

    Feature: invoice cancelling

--- src/components/Purchases/index.tsx
+++ src/components/Purchases/index.tsx
@@ -1,50 +1,146 @@
 import { Order } from "@/interfaces/user";
-import React from "react";
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import Button from "../Button";
+import { STATUS } from "@/constants";
+import logo from "@assets/logo-mask.png";
+import Image from "next/image";
+import Swal from "sweetalert2";
 
 type Props = {
   purchases: Order[];
 };
 
 function Purchases({ purchases }: Props) {
+  const { data: session } = useSession();
+  const [purchaseData, setPurchaseData] = useState(purchases);
+
+  useEffect(() => {
+    setPurchaseData(purchases);
+  }, [purchases]);
+
+  const handleCancelInvoiceClick = async (idInvoice: number) => {
+    const result = await Swal.fire({
+      title: 'Are you sure?',
+      text: 'This action cannot be undone.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, remove it!',
+      cancelButtonText: 'Cancel',
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+    });
+  
+    if (result.isConfirmed) {
+      console.log({ idInvoice });
+      const res = await fetch(`/api/users/purchases/${idInvoice}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${session?.user.token}`,
+        },
+      });
+      if (res.ok) {
+        fetchUpdatedPurchases();
+        Swal.fire({
+          icon: "success",
+          title: `Invoice n°${idInvoice} successfully cancelled`,
+          toast: true,
+          position: "bottom-end",
+          showConfirmButton: false,
+          timer: 3000,
+        });
+      } else {
+        Swal.fire('Error', 'Failed to cancel the invoice.', 'error');
+      }
+    }
+  };
+
+  const fetchUpdatedPurchases = async () => {
+    try {
+      const response = await fetch(
+        `/api/users/purchases/${session?.user.user_id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${session?.user.token}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const updatedPurchases = await response.json();
+        setPurchaseData(updatedPurchases);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <section className="w-full rounded p-2 border mb-8">
-      <h3 className="border-b-2 border-purple-300 py-2 mb-4 text-2xl font-bold">
+      <h3 className="border-b-2 border-purple-300 py-2 text-2xl font-bold">
         Last Purchases
       </h3>
 
-      <ul className="grid grid-cols-1 gap-4 px-4">
-        {!purchases.length ? (
+      <ul className="grid grid-cols-1 px-4">
+        {!purchaseData.length ? (
           <h3 className="text-2xl text-center py-8">No data</h3>
         ) : (
-          purchases.map((purchase, index) => (
+          purchaseData.map((purchase, index) => (
             <li
               key={index}
-              className="flex flex-col md:flex-row justify-between bg-gray-100 p-8 rounded border border-orange-2"
+              className="flex flex-col justify-between bg-gray-100 py-6 md:p-8 border-b border-orange-2 gap-2 md:gap-8"
             >
-              <div>
-                <p className="font-bold text-xl">
-                  Invoice n° {purchase.no_invoice}
+              <div className="flex justify-between">
+                <div className="flex flex-col">
+                  <p className="font-bold text-xl md:text-2xl">
+                    Invoice n° {purchase.no_invoice}
+                  </p>
+                  <p className="italic text-sm md:text-sm">
+                    Status: {purchase.status}
+                  </p>
+                  <p className="text-grey text-sm">{purchase.shippingCity}</p>
+                  <small>
+                    Shipping cost: $ {purchase.shippingcost.toFixed(2)}
+                  </small>
+                </div>
+                <p className="mt-2 font-bold md:text-xl text-end">
+                  Total: ${purchase.total.toFixed(2)}
                 </p>
-                <p className="italic text-gray-600">
-                  Status: {purchase.status}
-                </p>
-                <p className="text-grey">{purchase.shippingCity}</p>
               </div>
-              <p className="mt-2 font-bold">
-                Total: ${purchase.total.toFixed(2)}
-              </p>
-              <ul className="w-full md:w-1/2 mt-4">
-                {purchase.items.map((item, i) => (
-                  <li
-                    key={i}
-                    className="flex border-b border-lightGrey py-2 gap-4"
-                  >
-                    <span>{item.quantity} un.</span>
-                    <span className="w-full md:w-1/2">{item.model}</span>
-                    <span className="m-auto">${item.pxQ.toFixed(2)}</span>
-                  </li>
-                ))}
-              </ul>
+              <div className="flex flex-col md:flex-row justify-between">
+                <div className="">
+                  <p>Details:</p>
+                  <ul className="w-full md:w-96">
+                    {purchase.items.map((item, i) => (
+                      <li
+                        key={i}
+                        className="flex justify-between items-center gap-8 md:ml-6"
+                      >
+                        <div className="flex items-center gap-4">
+                          <Image
+                            src={logo}
+                            alt="costume mania bullet"
+                            width={20}
+                            height={20}
+                          />
+                          <span>{item.quantity} un.</span>
+                          <span>{item.model}</span>
+                        </div>
+                        <span>${item.pxQ.toFixed(2)}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                {purchase.status === STATUS.IN_PROGRESS && (
+                  <Button
+                    buttonStyle="secondary"
+                    onClick={() =>
+                      handleCancelInvoiceClick(purchase.no_invoice)
+                    }
+                    label="Cancel Order"
+                    className="self-center mt-8 md:mt-0"
+                  />
+                )}
+              </div>
             </li>
           ))
         )}
--- src/pages/api/users/purchases/[id].ts
+++ src/pages/api/users/purchases/[id].ts
@@ -4,30 +4,64 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const authorizationHeader = req.headers.authorization;
+
+  console.log(authorizationHeader)
+
+  if (!authorizationHeader) {
+    res.status(401).json({ message: "Authorization header missing" });
+    return;
+  }
+
   if (req.method === "GET") {
-    const authorizationHeader = req.headers.authorization;
-
-    if (!authorizationHeader) {
-      res.status(401).json({ message: "Authorization header missing" });
-      return;
-    }
-
     try {
-      const { id } = req.query
+      const { id } = req.query;
 
-      const response = await fetch(`${process.env.PRODUCT_API_URL}/sale/invoice/user/${id}`, {
-        headers: {
-          "Authorization": authorizationHeader,
-        },
-      });
-      
+      const response = await fetch(
+        `${process.env.PRODUCT_API_URL}/sale/invoice/user/${id}`,
+        {
+          headers: {
+            Authorization: authorizationHeader,
+          },
+        }
+      );
+
       if (!response.ok) {
-        console.error(response)
+        console.error(response);
         throw new Error("Service unavailable");
       }
 
       const data = await response.json();
-      
+
+      res.status(response.status).json(data);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } else if (req.method === "PUT") {
+    try {
+      const { id } = req.query;
+
+      const response = await fetch(
+        `${process.env.PRODUCT_API_URL}/sale/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: authorizationHeader,
+          },
+        }
+      );
+
+      console.log(response)
+
+      if (!response.ok) {
+        console.error(response);
+        throw new Error("Service unavailable");
+      }
+
+      const data = await response.json();
+
+      console.log(data)
+
       res.status(response.status).json(data);
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
