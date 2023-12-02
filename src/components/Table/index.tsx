6e9939038dc6fc048a6006a9a40d13d0a7ceeb45
Jeisson Lopez <jeisonma86@hotmail.com>
2023-12-01 19:22:39 -0500

    Adición de la tabla para la página de administrador

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { useTable, useSortBy, usePagination } from 'react-table';
+
+
+export type Column = {
+    Header: string;
+    accessor: any; // el nombre de la propiedad
+};
+
+
+function Table({ columns, data }: { columns: Column[], data: Object[] }) {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page, // en lugar de 'rows', usamos 'page'
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+    } = useTable(
+        {
+            columns,
+            data,
+            initialState: { pageIndex: 0 },
+        },
+        useSortBy,
+        usePagination
+    );
+
+    // Renderiza la interfaz de usuario de la tabla
+    return (
+        <>
+            <table {...getTableProps()} className="table-auto">
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())} className="px-4 py-2" key={column.id}>
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                                    </span>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map(row => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()} key={row.id}>
+                                {row.cells.map((cell, index) => (
+                                    <td {...cell.getCellProps()} className="border px-4 py-2" key={index}>{cell.render('Cell')}</td>
+                                ))}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Página{' '}
+                    <strong>
+                        {pageIndex + 1} de {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Ir a la página:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                            gotoPage(page);
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value));
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Mostrar {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </>
+    );
+}
+
+export default Table;
---
+++ src/interfaces/catalog.ts
@@ -0,0 +1,47 @@
+export interface Catalog {
+    idCatalog: number
+    model: Model
+    size: Size
+    stock: number
+    price: number
+    statusCatalog: Status
+    status: Status
+}
+
+export interface Model {
+    idModel: number
+    nameModel: string
+    category: Category
+    urlImage: string
+    statusModel: Status
+    status: Status
+}
+
+export interface Category {
+    idCategory: number
+    name: string
+    statusCategory: Status
+    status: Status
+}
+
+
+export interface Size {
+    id: number
+    adult: number
+    noSize: string
+    sizeDescription: string
+}
+
+export interface Status {
+    id: number
+}
+
+export interface CatalogDataTable {
+    id: number,
+    model: string,
+    adult: string,
+    size: string,
+    stock: number,
+    price: number,
+    category: string
+}
