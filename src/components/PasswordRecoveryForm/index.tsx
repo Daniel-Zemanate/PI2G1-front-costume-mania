2bcada4614f57cdfeee17334e2aa21949d831cbd
mlrosas21 <martinrosasortmann@gmail.com>
2023-12-06 10:55:00 -0300

    password reset functionality

--- src/components/PasswordRecoveryForm/index.tsx
+++ src/components/PasswordRecoveryForm/index.tsx
@@ -5,6 +5,10 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import Form from "../Form";
 import Button from "../Button";
+import { useSession } from "next-auth/react";
+import Swal from "sweetalert2";
+import { useRouter } from "next/router";
+import NavLink from "../NavLink/NavLink";
 
 const PasswordRecoverySchema = yup.object().shape({
   email: yup
@@ -14,7 +18,7 @@ const PasswordRecoverySchema = yup.object().shape({
   emailConfirmation: yup
     .string()
     .email("Enter a valid email")
-    .oneOf([yup.ref("password"), undefined], "Emails must match")
+    .oneOf([yup.ref("email"), undefined], "Emails must match")
     .required(),
 });
 
@@ -25,12 +29,58 @@ function PasswordRecoveryForm() {
     formState: { errors },
   } = useForm({ resolver: yupResolver(PasswordRecoverySchema) });
 
-  const onSubmit = handleSubmit((data) => console.log(data));
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const onSubmit = handleSubmit(async (data) => {
+    const { email } = data;
+
+    const body = {
+      email,
+    };
+
+    const response = await fetch("/api/users/reset", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        Authorization: `Bearer ${session?.user.token}`,
+      },
+    });
+
+    const { message } = await response.json();
+
+    console.log(response);
+    console.log(response.statusText);
+    console.log(message);
+
+    Swal.fire({
+      icon: response.ok ? "success" : "error",
+      title: response.ok ? "Success!" : "Error",
+      text: `${message}`,
+    });
+
+    if (response.ok) router.push("/");
+  });
 
   return (
-    <Form onSubmit={onSubmit} className="bg-purple-3 bg-opacity-25 px-16 py-8 my-8 rounded-lg flex flex-col max-w-screen-lg shadow min-w-[33%] m-auto">
+    <Form
+      onSubmit={onSubmit}
+      className="bg-purple-3 bg-opacity-25 px-16 py-8 my-8 rounded-lg flex flex-col max-w-screen-lg shadow min-w-[33%] m-auto"
+    >
       <Form.Header className="text-center p-4">
-        <Form.Title className="text-2xl font-bold">Recover your password</Form.Title>
+        <Form.Title className="text-2xl font-bold">
+          Recover your password
+        </Form.Title>
+        <Form.TextSection className="mt-4 flex justify-center gap-4">
+          <span>
+            Don&apos;t have an account?{" "}
+            <NavLink
+              label="Sign up"
+              route="/auth/signup"
+              textColor="purple-2"
+            />
+          </span>
+        </Form.TextSection>
       </Form.Header>
       <Form.Body register={register} className="flex flex-col justify-center">
         <FormInput
---
+++ src/pages/api/users/reset.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+
+    console.log(req.body)
+    const response = await fetch(
+      `${process.env.PRODUCT_API_URL}/users/reset`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: req.body,
+      }
+    );
+
+    if(response.ok){
+        res.status(200).json({message: "A temporary password will be sent to your inbox. Upon login, you'll have the option to change this password in 'My Account' section."});
+    } else {
+        res.status(400).json({message: "The email does not belong to any registered account. Please, sign up in the corresponding page"})
+    }
+  } else {
+    res.status(400).json({ message: "Método no permitido" });
+  }
+}
