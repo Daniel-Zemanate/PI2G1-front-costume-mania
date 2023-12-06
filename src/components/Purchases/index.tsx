32b1f29d1d878be86646baf705ee257b9d8208a7
mlrosas21 <martinrosasortmann@gmail.com>
2023-12-06 14:06:47 -0300

    style change

--- src/components/Purchases/index.tsx
+++ src/components/Purchases/index.tsx
@@ -75,13 +75,13 @@ function Purchases({ purchases }: Props) {
 
   return (
     <section className="w-full rounded p-2 border mb-8">
-      <h3 className="border-b-2 border-purple-300 py-2 text-2xl font-bold">
+      <h3 className="border-b-2 border-purple-3 py-2 mb-4 text-2xl font-bold">
         Last Purchases
       </h3>
 
       <ul className="grid grid-cols-1 px-4">
         {!purchaseData.length ? (
-          <h3 className="text-2xl text-center py-8">No data</h3>
+          <h3 className="text-2xl text-center py-8">No purchases yet</h3>
         ) : (
           purchaseData.map((purchase, index) => (
             <li
